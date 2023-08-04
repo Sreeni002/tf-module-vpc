8493ef34d58a54a3d3841620cdc5619f3286b948
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-04 09:10:51 +0530

    Peering connection

--- main.tf
+++ main.tf
@@ -21,13 +21,12 @@ module "subnets" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge(var.tags, { Name = "${var.env}- igw"})
+  tags = merge(var.tags, { Name = "${var.env}-igw"})
 }
 
 resource "aws_eip" "ngw" {
   count = length(var.subnets["public"].cidr_block)
-  //vpc = true
-  tags = merge(var.tags, { Name = "${var.env}- ngw"})
+  tags = merge(var.tags, { Name = "${var.env}-ngw"})
 }
 
 resource "aws_nat_gateway" "ngw" {
@@ -35,17 +34,17 @@ resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.ngw[count.index].id
   subnet_id     = module.subnets["public"].subnet_ids[count.index]
 
-  tags = merge(var.tags, { Name = "${var.env}- ngw"})
+  tags = merge(var.tags, { Name = "${var.env}-ngw"})
 }
 
-resource "aws_route" "igwrt" {
+resource "aws_route" "igw" {
   count = length(module.subnets["public"].route_table_ids)
   route_table_id = module.subnets["public"].route_table_ids[count.index]
   gateway_id = aws_internet_gateway.igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route" "ngwrt" {
+resource "aws_route" "ngw" {
   count = length(local.all_private_subnet_ids)
   route_table_id = local.all_private_subnet_ids[count.index]
   nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
@@ -54,7 +53,6 @@ resource "aws_route" "ngwrt" {
 
 
 resource "aws_vpc_peering_connection" "peer" {
-  peer_owner_id = data.aws_caller_identity.identity.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept = true
